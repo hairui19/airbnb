14ecc5cc24b90aa8a64a130b5e9d3e0bddd35277
hairuilin <linhairui19@gmail.com>
2023-02-15 12:45:14 +0800

    adding left and center navigation bar components

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,23 @@
-import Image from "next/image"
+import Image from "next/legacy/image"
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
     return (
-        <header>
-            <h1>I am the header</h1>
-
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5">
             {/* left */}
-            <div>
+            <div className="relative flex items-center h-10 cursor-pointer my-auto">
                 <Image src="https://links.papareact.com/qd3" 
                 layout="fill"
+                objectFit="contain"
+                objectPosition="left"
                 />
             </div>
 
-            {/* middle */}
-            <div></div>
+            {/* middle - search*/}
+            <div className="flex items-center border-2 rounded-full py-2">
+                <input className="flex-grow pl-5 bg-transparent outline-none" type="text" placeholder="start your search"/>
+                <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer mx-2" />
+            </div>
 
             {/* right */}
             <div></div>

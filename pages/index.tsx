d26af4276fb0e16a36e2e143886eeca44a4c7f2e
hairuilin <linhairui19@gmail.com>
2023-02-15 22:22:15 +0800

    some error on using getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,19 @@ import Header from '../components/Header'
 import Head from 'next/head'
 import Image from 'next/image'
 import Banner from '../components/Banner'
+import { InferGetStaticPropsType } from 'next';
 
-const Home: NextPage = () => {
+interface ExploreItem {
+  image: string,
+    location: string,
+    distance: string 
+}
+
+interface ExploreData {
+  items: ExploreItem[]
+}
+
+const Home: NextPage<ExploreData> = ( exploreData : ExploreData) => {
   return (
     <div className="">
       <Head>
@@ -12,11 +23,36 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      
       <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className='pt-6'>
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          {/* pull something from the server */}
+          { exploreData.items.map( item => (
+            <h1>{item.location}</h1>
+          ))}
+        </section>
+      </main>
     </div>
   )
 }
 
 export default Home
+
+export async function getStaticProps() {
+  const res = await fetch("https://links.papareact.com/pyp");
+  const exploreItems: ExploreItem[] = await res.json();
+
+  const exploreData: ExploreData = {
+    items: exploreItems
+  }
+
+  return {
+    props: {
+      exploreData
+    }
+  }
+}

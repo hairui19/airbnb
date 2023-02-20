0e3d5ad2fa478f5614d1053132026e9ae0557476
hairuilin <linhairui19@gmail.com>
2023-02-20 21:05:53 +0800

    formatted header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import Image from "next/legacy/image"
-import { 
-    MagnifyingGlassIcon, 
+import {
+    MagnifyingGlassIcon,
     GlobeAltIcon,
     Bars3Icon,
     UserCircleIcon
@@ -11,27 +11,27 @@ const Header = () => {
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5">
             {/* left */}
             <div className="relative flex items-center h-10 cursor-pointer my-auto">
-                <Image src="https://links.papareact.com/qd3" 
-                layout="fill"
-                objectFit="contain"
-                objectPosition="left"
+                <Image src="https://links.papareact.com/qd3"
+                    layout="fill"
+                    objectFit="contain"
+                    objectPosition="left"
                 />
             </div>
 
             {/* middle - search*/}
             <div className="flex items-center border-2 rounded-full py-2">
-                <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="start your search"/>
+                <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="start your search" />
                 <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer mx-2" />
             </div>
 
             {/* right */}
             <div className="flex items-center space-x-4 justify-end text-gray-500">
                 <p className="hidden md:inline cursor-pointer">Become a host</p>
-                <GlobeAltIcon className="h-6 cursor-pointer"/>
+                <GlobeAltIcon className="h-6 cursor-pointer" />
 
                 <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
-                    <Bars3Icon className="h-6"/>
-                    <UserCircleIcon className="h-6"/>
+                    <Bars3Icon className="h-6" />
+                    <UserCircleIcon className="h-6" />
                 </div>
             </div>
 

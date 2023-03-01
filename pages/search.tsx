09c356909699d3f575b9379d4721a9b281665cfd
hairuilin <linhairui19@gmail.com>
2023-03-01 22:02:36 +0800

    extract query values from router.query

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,18 +1,27 @@
+import { useRouter } from "next/router"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
+import { format } from "date-fns"
 
 const Search = () => {
+    const router = useRouter();
+    const { location, startDate, endDate, numberOfGuests } = router.query
+
+    const formattedStarDate = format(new Date(startDate as string), "dd MMMM yy")
+    const formattedEndDate = format(new Date(endDate as string), "dd MMMM yy")
+    const range = `${formattedStarDate} - ${formattedEndDate}`
+
     return (
         <div>
             <Header />
             <main className="flex">
                 <section className="flex-grow pt-14 px-6">
                     <p className="text-xs">
-                        300+ Stays for 5 number of guests
+                        300+ Stays - {range} - for {numberOfGuests} number of guests
                     </p>
 
                     <h1 className="text-3xl font-semibold mt-2 mb-6">
-                        Stays in New York
+                        Stays in {location}
                     </h1>
 
                     <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
@@ -32,6 +41,7 @@ const Search = () => {
 interface SearchFilterProps {
     title: string
 }
+
 const SearchFilter = ({ title }: SearchFilterProps) => {
     return (
         <p className="px-4 py-2 border rounded-full cursor-pointer 

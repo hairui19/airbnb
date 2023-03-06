4bc9518bc4ab776254dbaae3b639b30a51c3f06a
hairuilin <linhairui19@gmail.com>
2023-03-06 14:29:26 +0800

    successfully create an api in flutter

---
+++ pages/api/flutter.ts
@@ -0,0 +1,47 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  // res.status(200).json({ name: `${req.method}` } )
+  const fetchOptions = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key": process.env.MONGODB_DATA_API_KEY,
+    },
+  };
+
+  const fetchBody = {
+    dataSource: process.env.MONGODB_DATA_SOURCE,
+    database: "REVIEW_BUTTERFLY",
+    collection: "reviews",
+  };
+
+  const baseUrl = `${process.env.MONGODB_DATA_API_URL}/action`;
+
+  try {
+    switch (req.method) {
+      case "GET":
+        const readDataJson = await fetch(`${baseUrl}/find`, {
+          ...fetchOptions,
+          body: JSON.stringify({
+            ...fetchBody
+          }),
+        }).then(res => res.json())
+        res.status(200).json(readDataJson.documents)
+        break;
+      default:
+        res.status(405).end();
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+  }
+}

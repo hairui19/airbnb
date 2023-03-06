1bb8e07b0cee75aa479ff6c6232628139b15a861
hairuilin <linhairui19@gmail.com>
2023-03-06 18:20:10 +0800

    some minor refactoring

--- pages/api/flutter.ts
+++ pages/api/flutter.ts
@@ -18,24 +18,32 @@ export default async function handler(
     },
   };
 
-  const fetchBody = {
-    dataSource: process.env.MONGODB_DATA_SOURCE,
-    database: "REVIEW_BUTTERFLY",
-    collection: "reviews",
-  };
-
   const baseUrl = `${process.env.MONGODB_DATA_API_URL}/action`;
 
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key": process.env.MONGODB_DATA_API_KEY,
+    },
+    body: JSON.stringify({
+      dataSource: process.env.MONGODB_DATA_SOURCE,
+      database: "REVIEW_BUTTERFLY",
+      collection: "reviews",
+    }),
+  };
   try {
     switch (req.method) {
       case "GET":
-        const readDataJson = await fetch(`${baseUrl}/find`, {
-          ...fetchOptions,
-          body: JSON.stringify({
-            ...fetchBody
-          }),
-        }).then(res => res.json())
-        res.status(200).json(readDataJson.documents)
+        const readDataJson = await fetch(`${baseUrl}/find`, options).then(
+          (res) => res.json()
+        );
+        res.status(200).json(readDataJson.documents);
+        break;
+      case "POST":
+        const review = req.body;
+        const insertData = await fetch(`${baseUrl}/insertOne`, {});
         break;
       default:
         res.status(405).end();

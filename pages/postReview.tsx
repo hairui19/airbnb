96e3cd4b2423309d314ee4c2b2613883a4ab8c4f
hairuilin <linhairui19@gmail.com>
2023-03-12 14:48:31 +0800

    add reviewtext as condition of upload

--- pages/postReview.tsx
+++ pages/postReview.tsx
@@ -54,7 +54,7 @@ const PostReview = () => {
                 </div>
 
                 <div className="mt-5 sm:mt-6">
-                    <button onClick={handleUploadImage} disabled={!imageUpload} type="button" className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 
+                    <button onClick={handleUploadImage} disabled={!imageUpload || reviewtext.length < 15} type="button" className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 
                     focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:cursor-not-allowed disabled:bg-gray-300">
                         Upload Review
                     </button>

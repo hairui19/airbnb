a043567a67d252b0a664c20e41138664a8c25df7
hairuilin <linhairui19@gmail.com>
2023-03-13 16:01:13 +0800

    refactored explore page

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -26,45 +26,19 @@ const Explore = ({ reviews }: ExplorePageProps) => {
             <main className=" max-w-7xl mx-auto px-8 sm:px-1 pt-6 flex justify-center">
                 <div className="inline-grid grid-cols-3 grid-flow-auto gap-x-[1.5rem] auto-cols-max">
                     {/** Flex boxes */}
-                    <div className="inline-flex flex-col gap-10">
-                        {reviews
-                            .filter((_, index) => index % 3 == 0)
-                            .map((review) => {
-                                return (
+                    {[0, 1, 2].map((colIndex) => (
+                        <div key={colIndex} className="inline-flex flex-col gap-10">
+                            {reviews
+                                .filter((_, index) => index % 3 === colIndex)
+                                .map((review) => (
                                     <ReviewCard
                                         key={review[0]}
                                         review={review[1]}
-                                        onClick={() => { onReviewClick(review[0]) }}
+                                        onClick={() => onReviewClick(review[0])}
                                     />
-                                )
-                            })}
-                    </div>
-                    <div className="inline-flex flex-col gap-10">
-                        {reviews
-                            .filter((_, index) => index % 3 == 1)
-                            .map((review) => {
-                                return (
-                                    <ReviewCard
-                                        key={review[0]}
-                                        review={review[1]}
-                                        onClick={() => { onReviewClick(review[0]) }}
-                                    />
-                                )
-                            })}
-                    </div>
-                    <div className="inline-flex flex-col gap-10">
-                        {reviews
-                            .filter((_, index) => index % 3 == 2)
-                            .map((review) => {
-                                return (
-                                    <ReviewCard
-                                        key={review[0]}
-                                        review={review[1]}
-                                        onClick={() => { onReviewClick(review[0]) }}
-                                    />
-                                )
-                            })}
-                    </div>
+                                ))}
+                        </div>
+                    ))}
                 </div>
             </main>
         </div>
